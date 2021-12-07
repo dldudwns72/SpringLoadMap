c05d11a027497d1ebd6a2cb60eea28306a78d8e3
LeeYoungJun <dldudwns72@gmail.com>
2021-12-08 01:09:00 +0900

    다양한 매핑을 통한 컨트롤러 요청

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,74 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    // method 미지정 시 어떠한 요청으로 와도 다 성공한다.
+    @RequestMapping(value = {"/hello-basic","/hello-basic-two"},method = RequestMethod.GET)
+    public String helloBasic(){
+        log.info("helloBasic");
+        return "OK";
+    }
+
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    // 파라미터에에 mode=debug 가 필수로 있어야한다
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    // 헤더에 mode=debug 가 필수로 있어야한다
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    // Content-Type 헤더 기반 추가 매핑
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+
+
+
+}
