aefa42527cc6dc8b0573b2995bded71f571c17dc
LeeYoungJun <dldudwns72@gmail.com>
2021-12-17 00:41:16 +0900

    Controller에서 Session 적용

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
@@ -20,13 +22,14 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 //    @GetMapping("/")
 //    public String home() {
 //        return "home";
 //    }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if (memberId == null) {
             return "home";
@@ -42,16 +45,18 @@ public class HomeController {
         return "loginHome";
     }
 
-    @PostMapping("/logout")
-    public String logout(HttpServletResponse response) {
-        expireCookie(response, "memberId");
-        return "redirect:/";
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+        if (member == null) {
+            return "home";
+        }
+        //로그인
+        model.addAttribute("member", member);
+        return "loginHome";
     }
 
-    private void expireCookie(HttpServletResponse response, String cookieName) {
-        Cookie cookie = new Cookie(cookieName, null);
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-    }
+
 
 }
