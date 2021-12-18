94a5f8b40072160dbb470214f3e02d550b5a5b4f
LeeYoungJun <dldudwns72@gmail.com>
2021-12-18 11:34:21 +0900

    filter를 통한 servlet 요청 전 플로우 확인 및 filter 등록

---
+++ login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,23 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    // WAS 를 띄울때 filter 를 등록해 준다.
+    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
