fec74e9664bf51e777689672e02a68af38677159
LeeYoungJun <dldudwns72@gmail.com>
2021-12-18 13:39:39 +0900

    filter를 통한 로그인 실패 시 redirect 기능 구현

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.*;
 import javax.validation.Valid;
@@ -105,7 +106,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
+//    @PostMapping("/logout")
     public String logoutV3(HttpServletRequest request) {
         //세션을 삭제한다.
         HttpSession session = request.getSession(false);
@@ -113,6 +114,32 @@ public class LoginController {
             session.invalidate();
         }
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String loginV4(
+            @Valid @ModelAttribute LoginForm form,
+            BindingResult bindingResult,
+            @RequestParam(defaultValue = "/") String redirectURL,
+            HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(),
+                form.getPassword());
+        log.info("login? {}", loginMember);
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+        //로그인 성공 처리
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+        //redirectURL 적용
+        return "redirect:" + redirectURL;
     }
 
 
