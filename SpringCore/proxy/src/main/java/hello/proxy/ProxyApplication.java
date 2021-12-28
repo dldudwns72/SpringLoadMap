2f96ee62a5f4d2d6f2e61240acd7883833999062
LeeYoungJun <dldudwns72@gmail.com>
2021-12-29 00:46:07 +0900

    인터페이스를 이용한 구현체를 만들어 등록

--- SpringCore/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringCore/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,13 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import(AppV1Config.class) // AppV1Config.class 를 스프링 빈으로 등록한다.
+//주의, app과 app 하위에서만 componentScan 을 진행하도록 설정 AppV1Config는 따로 빈으로 인식하기 위해
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
 	public static void main(String[] args) {
