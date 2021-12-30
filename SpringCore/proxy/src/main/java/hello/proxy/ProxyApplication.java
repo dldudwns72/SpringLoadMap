9f2d45c357a20d1f565d3f2e099c80892a17486e
LeeYoungJun <dldudwns72@gmail.com>
2021-12-31 01:00:37 +0900

    동적 Proxy 패턴을 이용한 로그 추적기 설정

--- SpringCore/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringCore/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,8 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +15,9 @@ import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class,AppV2Config.class}) // 스프링 빈으로 등록한다.
 //@Import(InterfaceProxyConfig.class) // 스프링 빈으로 등록한다.
-@Import(ConcreteProxyConfig.class) // 스프링 빈으로 등록한다.
+//@Import(ConcreteProxyConfig.class) // 스프링 빈으로 등록한다.
+//@Import(DynamicProxyBasicConfig.class) // 스프링 빈으로 등록한다.
+@Import(DynamicProxyFilterConfig.class) // 스프링 빈으로 등록한다.
 //주의, app과 app 하위에서만 componentScan 을 진행하도록 설정 AppV1Config는 따로 빈으로 인식하기 위해
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
