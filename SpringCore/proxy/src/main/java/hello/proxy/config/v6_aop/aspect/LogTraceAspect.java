4bbaa7afce7ad72163275a7432d6ba1a7be1a34b
LeeYoungJun <dldudwns72@gmail.com>
2022-01-02 03:30:28 +0900

    @Aspect를 사용하여 애노테이편 기반 프록시 설정

---
+++ SpringCore/proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,43 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class LogTraceAspect {
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    /*
+        PointCut과 Advice를 합쳐서 Advisor을 만든다.
+     */
+    @Around("execution(* hello.proxy.app..*(..))") // PointCut
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        // Advice
+
+        TraceStatus status = null;
+        // log.info("target={}", joinPoint.getTarget()); //실제 호출 대상
+        // log.info("getArgs={}", joinPoint.getArgs()); //전달인자
+        // log.info("getSignature={}", joinPoint.getSignature()); //join point 시그니처
+
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+            //로직 호출
+            Object result = joinPoint.proceed();
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
