fea9a102649ed9c4dac1408c1f6681261baf2333
LeeYoungJun <dldudwns72@gmail.com>
2022-01-01 15:27:36 +0900

    N개의어드바이저를 적용한 프록시 팩토리 적용

--- SpringCore/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ SpringCore/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,8 @@ import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +19,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class) // 스프링 빈으로 등록한다.
 //@Import(ConcreteProxyConfig.class) // 스프링 빈으로 등록한다.
 //@Import(DynamicProxyBasicConfig.class) // 스프링 빈으로 등록한다.
-@Import(DynamicProxyFilterConfig.class) // 스프링 빈으로 등록한다.
+//@Import(DynamicProxyFilterConfig.class) // 스프링 빈으로 등록한다.
+//@Import(ProxyFactoryConfigV1.class) // 스프링 빈으로 등록한다.
+@Import(ProxyFactoryConfigV2.class) // 스프링 빈으로 등록한다.
 //주의, app과 app 하위에서만 componentScan 을 진행하도록 설정 AppV1Config는 따로 빈으로 인식하기 위해
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
