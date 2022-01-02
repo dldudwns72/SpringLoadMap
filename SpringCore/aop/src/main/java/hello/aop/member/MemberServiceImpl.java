3b66918b8389b473223b89ceb60a6645b186a155
LeeYoungJun <dldudwns72@gmail.com>
2022-01-02 23:48:27 +0900

    member package, member.annotation 패키지 변경

--- SpringCore/aop/src/main/java/hello/aop/member/MemberServiceImpl.java
+++ SpringCore/aop/src/main/java/hello/aop/member/MemberServiceImpl.java
@@ -1,12 +1,12 @@
-package hello.aop.order.aop.member.annotation;
+package hello.aop.member;
 
-import hello.aop.order.aop.member.ClassAop;
-import hello.aop.order.aop.member.MethodAop;
+import hello.aop.member.annotation.ClassAop;
+import hello.aop.member.annotation.MethodAop;
 import org.springframework.stereotype.Component;
 
 @ClassAop
 @Component
-public class MemberServiceImpl implements MemberService{
+public class MemberServiceImpl implements MemberService {
 
     @Override
     @MethodAop("test value")
--- SpringCore/aop/src/test/java/hello/aop/pointcut/ArgsTest.java
+++ SpringCore/aop/src/test/java/hello/aop/pointcut/ArgsTest.java
@@ -1,6 +1,6 @@
 package hello.aop.pointcut;
 
-import hello.aop.order.aop.member.annotation.MemberServiceImpl;
+import hello.aop.member.MemberServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
