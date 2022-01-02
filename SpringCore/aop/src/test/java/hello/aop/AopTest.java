4d6193f21683c8faf07601810de1fb420296f27b
LeeYoungJun <dldudwns72@gmail.com>
2022-01-02 13:21:39 +0900

    @Pointcut 부분을 외부 클래스로 정의하여 적용

--- SpringCore/aop/src/test/java/hello/aop/AopTest.java
+++ SpringCore/aop/src/test/java/hello/aop/AopTest.java
@@ -5,6 +5,7 @@ import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -20,8 +21,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
-
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopTest {
 
     @Autowired
