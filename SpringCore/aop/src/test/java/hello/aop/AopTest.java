fb1d50b3d8f485be92d97c22c37a538eda08d981
LeeYoungJun <dldudwns72@gmail.com>
2022-01-02 14:00:06 +0900

    @Around 라이프사이클을 나눈 5가지 어노테이션 분리

--- SpringCore/aop/src/test/java/hello/aop/AopTest.java
+++ SpringCore/aop/src/test/java/hello/aop/AopTest.java
@@ -20,7 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 
 
