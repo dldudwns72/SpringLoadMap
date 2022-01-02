215863350ded94fde4b028d9618ba7a9a39f089d
LeeYoungJun <dldudwns72@gmail.com>
2022-01-02 12:57:32 +0900

    @Aspect를 이용한 객체 생성 전 proxy 패턴을 통한 로그 찍기 기본 예제

---
+++ SpringCore/aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,49 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+@Import(AspectV1.class)
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    @DisplayName("aop proxy 적용 여부 테스트")
+    void aopInfo() {
+        log.info("isAopProxy, orderService = {}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository = {}", AopUtils.isAopProxy(orderRepository));
+
+        assertThat(AopUtils.isAopProxy(orderService)).isTrue();
+        assertThat(AopUtils.isAopProxy(orderRepository)).isTrue();
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+}
