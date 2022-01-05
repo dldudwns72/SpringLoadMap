8c9df653b940d1f72ba1b90b847297c2219654d0
LeeYoungJun <dldudwns72@gmail.com>
2022-01-06 01:03:32 +0900

    JPA 기본 Study First Commit

---
+++ JPA Biggner/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,53 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        //persistence-unit
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 멤버 Insert
+            //Member member = new Member();
+            //member.setId(2L);
+            //member.setName("HelloB");
+            //em.persist(member);
+
+            // JPQL을 통한 쿼리를 통하여 데이터를 가져오기
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+            for(Member member : result){
+                System.out.println("member.name " + member.getName());
+            }
+
+            // 멤버 Read
+            //Member findMember = em.find(Member.class , 1L);
+            //System.out.println("findMember.id = " + findMember.getId());
+            //System.out.println("findMember.name = " + findMember.getName());
+
+            // 멤버 Update
+            //Member findMember = em.find(Member.class , 1L);
+            //findMember.setName("HelloJPA");
+
+            // 멤버 Delete
+            //Member findMember = em.find(Member.class , 1L);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
