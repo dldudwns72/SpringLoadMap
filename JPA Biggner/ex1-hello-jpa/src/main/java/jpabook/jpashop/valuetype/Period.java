2eaf771c2775536e226d57ba282f818e019385f8
LeeYoungJun <dldudwns72@gmail.com>
2022-01-09 02:59:49 +0900

    값 타입은 불변객체로 생성하여 관리

--- JPA Biggner/ex1-hello-jpa/src/main/java/jpabook/jpashop/valuetype/Period.java
+++ JPA Biggner/ex1-hello-jpa/src/main/java/jpabook/jpashop/valuetype/Period.java
@@ -6,9 +6,15 @@ import java.time.LocalDateTime;
 
 @Embeddable
 public class Period {
-
+    @Column(name = "START_DATE")
     private LocalDateTime startDate;
+
     private LocalDateTime endDate;
+
+    public Period(LocalDateTime startDate, LocalDateTime endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 
     public boolean isWork(){
         if(startDate.getHour() > endDate.getHour()){
@@ -17,20 +23,13 @@ public class Period {
         return false;
     }
 
-    @Column(name = "START_DATE")
     public LocalDateTime getStartDate() {
         return startDate;
-    }
-
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
     }
 
     public LocalDateTime getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
-    }
+
 }
