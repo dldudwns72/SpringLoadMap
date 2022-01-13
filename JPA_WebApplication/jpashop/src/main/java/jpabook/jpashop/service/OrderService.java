9cd62476987c8ddd957258d958c4b8b5141f7df8
LeeYoungJun <dldudwns72@gmail.com>
2022-01-13 23:55:25 +0900

    도메인 모델 패턴을 통한 주문 서비스 로직 구현

---
+++ JPA_WebApplication/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    // 주문
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+        Member findMember = memberRepository.findOne(memberId);
+        Item findItem = itemRepository.findOne(itemId);
+
+        // 배송 정보
+        Delivery delivery = new Delivery();
+        delivery.setAddress(findMember.getAddress());
+
+        // 주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(findItem, findItem.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(findMember, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+    // 취소
+    @Transactional
+    public void cancelOrder(Long orderId){
+        Order findOrder = orderRepository.findOne(orderId);
+
+        findOrder.cancel();
+    }
+
+    // 검색
+//    public List<Order> findOrders(OrderSearch orderSearchc
+}
