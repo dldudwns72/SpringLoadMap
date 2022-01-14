976b5950ad61db8d81e7b4f86c78e4f93fc4dfb6
LeeYoungJun <dldudwns72@gmail.com>
2022-01-15 02:12:02 +0900

    상품 주문, 주문목록 검색,취소

--- JPA_WebApplication/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ JPA_WebApplication/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -50,6 +51,7 @@ public class OrderService {
         findOrder.cancel();
     }
 
-    // 검색
-//    public List<Order> findOrders(OrderSearch orderSearchc
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAll(orderSearch);
+    }
 }
