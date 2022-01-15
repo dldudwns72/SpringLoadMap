32d24de93a2baccde4e809e2d2b5a7dd31234df2
LeeYoungJun <dldudwns72@gmail.com>
2022-01-15 12:07:12 +0900

    회원 조회 및 조회 시 Entity를 노출하지 않고 DTO를 통한 Response 반환

--- JPA_WebApplication/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ JPA_WebApplication/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,12 +8,44 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    // Response의 껍데기를 배열이 아닌 Object로 반환하기위해 따로 담는다. => 객체의 유연성을 높혀주기 위함
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
+
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -35,7 +67,9 @@ public class MemberApiController {
             @PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
 
         memberService.update(id, request.getName());
-        Member findMember = memberService.findOne(id);
+
+        // update 트랜잭션이 끝난 후 id를 찾아서 멤버를 조회한 후 DTO에 담아서 반환
+       Member findMember = memberService.findOne(id);
 
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
@@ -48,7 +82,6 @@ public class MemberApiController {
     @Data
     static class CreateMemberResponse {
         private Long id;
-
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
