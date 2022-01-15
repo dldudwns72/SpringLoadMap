854d74eb9393099af0d895f689acd4c40748484b
LeeYoungJun <dldudwns72@gmail.com>
2022-01-15 11:36:09 +0900

    회원 등록 API 패키지 위치 변경

--- JPA_WebApplication/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ JPA_WebApplication/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,10 +1,9 @@
-package jpabook.api;
+package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
