f1f3a065333f450c9d71d1e99af87e62efc10208
LeeYoungJun <dldudwns72@gmail.com>
2022-01-16 13:08:15 +0900

    Order Entity DTO 변환 - fetch 적용 전

--- JPA_WebApplication/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ JPA_WebApplication/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +21,7 @@ public class OrderApiController {
     private final OrderRepository orderRepository;
 
     // 엔티티 직접 호출
+    @GetMapping("api/v1/orders")
     public List<Order> ordersV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
 
@@ -30,4 +36,55 @@ public class OrderApiController {
 
         return all;
     }
+
+    @GetMapping
+    public List<OrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Getter
+    static class OrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            address = order.getDelivery().getAddress();
+
+            // OrderItem Entity를 그대로 가져와서 사용하면 안된다.
+            //order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+            //orderItems = order.getOrderItems();
+            orderItems = order.getOrderItems().stream()
+                    .map(oi -> new OrderItemDto(oi))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto{
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+
+
+
+
+
 }
